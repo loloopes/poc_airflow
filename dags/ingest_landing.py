9ae64c9691db545a508fc8df479fb002f9b6d7d0
Gustavo Lopes <gustavo.campos@ndtv.com.br>
2025-04-03 12:49:12 -0300

    adjust gitignore

--- dags/ingest_landing.py
+++ dags/ingest_landing.py
@@ -20,31 +20,35 @@ default_args = {
     tags=['save_from_excel', 'saves_from_mssql'],
     description='Loads all CSVs, data from mssql to S3 landing for further transformation',
     default_args=default_args,
-    dagrun_timeout=timedelta(seconds=20),
+    dagrun_timeout=timedelta(minutes=6),
     max_consecutive_failed_dag_runs=2,
 )
 def ingest_landing():
 
     @task()
+    def start_dag():
+        print(f"[I.N.F.O] Start dag!!!!!!!!!!!!!!!!!1")
+
+    @task()
     def ingest_raw_excel():
         _store_excel_file("/usr/local/airflow/temp/excel/")
 
-    # @task()
-    # def ingest_data_from_db():
-    #     _store_mssql_data()
+    # Store mssql data here
 
     @task()
     def ingest_landing_is_done():
         print(f"[I.N.F.O] Dag ingest_landing ran successfully!!!")
 
-    trigger_landing_to_bronze = TriggerDagRunOperator(
-        task_id="trigger_landing_to_bronze",
-        trigger_dag_id="landing_to_bronze",
-        wait_for_completion=False,  # Set to True if you want to wait for it to finish
-        reset_dag_run=True          # Replace existing run if one exists for that date
-    )
-    
-    # ingest_raw_excel() >> ingest_data_from_db() >> print_is_done()
-    ingest_raw_excel() >> ingest_landing_is_done() >> trigger_landing_to_bronze
+    # Define TriggerDagRunOperator inside DAG context
+    def _trigger_landing_to_bronze():
+        return TriggerDagRunOperator(
+            task_id="trigger_landing_to_bronze",
+            trigger_dag_id="landing_to_bronze",
+            wait_for_completion=False,
+            reset_dag_run=True
+        )
+
+    start_dag() >> ingest_raw_excel() >> ingest_landing_is_done() >> _trigger_landing_to_bronze()
 
 ingest_landing()
+
--- dags/landing_to_bronze.py
+++ dags/landing_to_bronze.py
@@ -1,7 +1,7 @@
-from airflow.decorators import dag, task
-from datetime import timedelta, datetime
-from airflow.providers.docker.operators.docker import DockerOperator
+from datetime import datetime, timedelta
 
+from airflow.decorators import dag, task
+from airflow.providers.docker.operators.docker import DockerOperator
 
 default_args = {
     'owner': 'loloopes',
@@ -23,28 +23,30 @@ def landing_to_bronze():
     def start_dag():
         print(f'[I.N.F.O] Dag landing_to_bronze_started')
 
-    run_transform_and_load = DockerOperator(
-        task_id="run_transform_and_load",
-        image="airflow/run_transform_and_load",  # name of the image you build for Excel > Delta
-        docker_url="tcp://docker-proxy:2375",
-        network_mode="container:spark-master",
-        container_name='format_excel',
-        api_version="auto",
-        auto_remove='success',
-        tty=True,
-        xcom_all=False,
-        mount_tmp_dir=False,
-        environment={
-            "AWS_ACCESS_KEY_ID": "minioadmin",
-            "AWS_SECRET_ACCESS_KEY": "minioadmin",
-            "ENDPOINT": "http://host.docker.internal:9000",
-        },
-    )
+    # run_transform_and_load = DockerOperator(
+    #     task_id="run_transform_and_load",
+    #     image="airflow/run_transform_and_load",  # name of the image you build for Excel > Delta
+    #     docker_url="tcp://docker-proxy:2375",
+    #     network_mode="container:spark-master",
+    #     container_name='format_excel',
+    #     api_version="auto",
+    #     auto_remove='success',
+    #     tty=True,
+    #     xcom_all=False,
+    #     mount_tmp_dir=False,
+    #     environment={
+    #         "AWS_ACCESS_KEY_ID": "minioadmin",
+    #         "AWS_SECRET_ACCESS_KEY": "minioadmin",
+    #         "ENDPOINT": "http://host.docker.internal:9000",
+    #         "SPARK_APPLICATION_ARGS": "landing/bike-shop"  
+    #     },
+    # )
 
     @task()
     def landing_to_bronze_is_done():
         print(f"[I.N.F.O] Dag landing_to_bronze ran successfully!!!")
 
-    start_dag() >> run_transform_and_load >> landing_to_bronze_is_done()
+    # start_dag() >> run_transform_and_load >> landing_to_bronze_is_done()
+    start_dag() >> landing_to_bronze_is_done()
 
 landing_to_bronze()
